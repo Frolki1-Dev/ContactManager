1065c8fed27063fab096ef7c253ab2d1935d723a
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2021-06-21 16:41:05 +0200

    Add model

---
+++ Contact_Manager/Models/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Manager.Models
+{
+    class Model
+    {
+        public bool insert()
+        {
+            return false;
+        }
+
+        public bool updateById(int rowId)
+        {
+            return false;
+        }
+
+        /**
+         * Set the deleted flag
+         */
+        public bool delete(int rowId)
+        {
+            return false;
+        }
+    }
+}
