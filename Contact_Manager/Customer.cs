cf3358cf851af5928faaf2aa088568b6d11cb2c2
BigCoc0nut <nicolasegli@outlook.com>
2021-06-27 19:14:40 +0200

    start customer

---
+++ Contact_Manager/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Manager
+{
+    class Customer
+    {
+        public Customer(string companyname, string c_address, int c_zipcode, string c_city, string c_name, string c_surname, int c_phonenumber, string c_mail)
+        {
+            Companyname = companyname;
+            C_Address = c_address;
+            C_Zipcode = c_zipcode;
+            C_City = c_city;
+            C_Name = c_name;
+            C_Surname = c_surname;
+            C_Phonenumber = c_phonenumber;
+            C_Mail = c_mail;
+
+
+        }
+
+        public string Companyname { get; set; }
+
+        public string C_Address { get; set; }
+
+        public int C_Zipcode { get; set; }
+
+        public string C_City { get; set; }
+
+        public string C_Name { get; set; }
+
+        public string C_Surname { get; set; }
+
+        public int C_Phonenumber { get; set; }
+
+        public string C_Mail { get; set; }
+
+
+
+
+
+
+    }
+
+}
