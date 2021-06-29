9b3cf736b8f5685e896049587bdfc7c9ebdd41e4
Fabian Dort <fabian@dort.sh>
2021-06-29 17:14:32 +0200

    Updated all classes with constructor, properties and naming convention

---
+++ Contact_Manager/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact_Manager
+{
+    class Customer : Person
+    {
+        //constructor
+        public Customer(string salutation, string firstName, string lastName, DateTime dateOfBirth, int gender, string title, string email, string status,
+            string address, int zipCode, string phonePrivate, string phoneCompany, string fax, string mobile, string city, string country, string companyName, string customerType, string companyContact)
+            : base(salutation, firstName, lastName, dateOfBirth, gender, title, email, status, address, zipCode, phonePrivate, phoneCompany, fax, mobile, city, country)
+        {
+            CompanyName = companyName;
+            CustomerType = customerType;
+            CompanyContact = companyContact;
+        }
+
+        //Properties
+        public string CompanyName { get; set; }
+        public string CustomerType { get; set; }
+        public string CompanyContact { get; set; }
+    }
+}
--- Contact_Manager/Person.cs
+++ Contact_Manager/Person.cs
@@ -11,21 +11,22 @@ namespace Contact_Manager
         // variables
 
         // constructor
-        public Person(string salutation, string first_name, string last_name, string date_of_birth, int gender, string title, string email, string status, string address, int zip_code, int phone_private, int phone_company, int fax, int mobile, string city, string country)
+        public Person(string salutation, string firstName, string lastName, DateTime dateOfBirth, int gender, string title, string email, string status,
+            string address, int zipCode, string phonePrivate, string phoneCompany, string fax, string mobile, string city, string country)
         {
             Salutation = salutation;
-            First_name = first_name;
-            Last_name = last_name;
-            Date_of_birth = date_of_birth;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
             Gender = gender;
             Title = title;
             Email = email;
             Status = status;
             Address = address;
-            Zip_code = zip_code;
+            ZipCode = zipCode;
             City = city;
-            Phone_private = phone_private;
-            Phone_company = phone_company;
+            PhonePrivate = phonePrivate;
+            PhoneCompany = phoneCompany;
             Fax = fax;
             Mobile = mobile;
             Country = country;
@@ -33,23 +34,23 @@ namespace Contact_Manager
 
         // properties
         public string Salutation { get; set; }
-        public string First_name { get; set; }
-        public string Last_name { get; set; }
-        public string Date_of_birth { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public int Gender { get; set; }
         public string Title { get; set; }
         public string Email { get; set; }
         public string Status { get; set; }
         public string Address { get; set; }
-        public int Zip_code { get; set; }
-        public int Phone_private { get; set; }
-        public int Phone_company { get; set; }
-        public int Fax { get; set; }
-        public int Mobile { get; set; }
+        public int ZipCode { get; set; }
+        public string PhonePrivate { get; set; }
+        public string PhoneCompany { get; set; }
+        public string Fax { get; set; }
+        public string Mobile { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
 
         // methods
-
+        // hier erfolgt der Speichervorgang in das ensprechende JSON-File
     }
 }
