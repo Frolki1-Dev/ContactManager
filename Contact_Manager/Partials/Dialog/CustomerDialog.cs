1efba4f6362f1fd596fc96ed5b53a9275434d759
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2021-08-22 11:24:32 +0200

    Fix some problems with customers. Add validation class.

--- Contact_Manager/Partials/Dialog/CustomerDialog.cs
+++ Contact_Manager/Partials/Dialog/CustomerDialog.cs
@@ -18,11 +18,11 @@ namespace Contact_Manager.Partials.Dialog
 {
     public partial class CustomerDialog : Form
     {
-        private Customer currentCustomer;
+        private Customer _currentCustomer;
 
         private BindingSource _source;
-        private bool noteInEditMode = false;
-        private int rowIndex = 0;
+        private bool _noteInEditMode = false;
+        private int _rowIndex = 0;
 
         public CustomerDialog()
         {
@@ -36,26 +36,28 @@ namespace Contact_Manager.Partials.Dialog
         {
              
             InitializeComponent();
-            currentCustomer = customer;
-            GrpBoxNotes.Visible = true;
+            CmbNationality.DataSource = CountryList();
+            _currentCustomer = customer;
+            btnCompanyDelete.Visible = false;
+            
 
             //load the customer to edit 
-            txtCompany.Text = currentCustomer.CompanyName;
-            txtAddress.Text = currentCustomer.Address;
-            txtZipCode.Text = currentCustomer.ZipCode.ToString();
-            txtCity.Text = currentCustomer.City;
-            txtFirstName.Text = currentCustomer.FirstName;
-            txtSurName.Text = currentCustomer.LastName;
-            txtPhonePrivate.Text = currentCustomer.PhonePrivate;
-            txtPhoneCompany.Text = currentCustomer.PhoneCompany;
-            txtMobile.Text = currentCustomer.Mobile;
-            txtFax.Text = currentCustomer.Fax;
-            txtCompanyContactEmail.Text = currentCustomer.Email;
+            txtCompany.Text = _currentCustomer.CompanyName;
+            txtAddress.Text = _currentCustomer.Address;
+            txtZipCode.Text = _currentCustomer.ZipCode.ToString();
+            txtCity.Text = _currentCustomer.City;
+            txtFirstName.Text = _currentCustomer.FirstName;
+            txtSurName.Text = _currentCustomer.LastName;
+            txtPhonePrivate.Text = _currentCustomer.PhonePrivate;
+            txtPhoneCompany.Text = _currentCustomer.PhoneCompany;
+            txtMobile.Text = _currentCustomer.Mobile;
+            txtFax.Text = _currentCustomer.Fax;
+            txtCompanyContactEmail.Text = _currentCustomer.Email;
 
             // TODO: Missing Kundentyp, Aktiver Kunde, Anrede, Titel, Geschlecht, Nationalität, Geburtsdatum
 
             // Build a new source
-            updateNotesView();
+            UpdateNotesView();
         }
 
        
@@ -63,7 +65,7 @@ namespace Contact_Manager.Partials.Dialog
         public static List<string> CountryList()
         {
             // Creating culture list
-            List<string> CultureList = new List<string>();
+            List<string> cultureList = new List<string>();
 
             // getting the specific CultureInfo from CultureInfo class
             CultureInfo[] getCultureInfos = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
@@ -74,36 +76,39 @@ namespace Contact_Manager.Partials.Dialog
                 RegionInfo getRegionInfo = new RegionInfo(getCulture.LCID);
 
                 // adding each country name into the arraylist
-                if (!(CultureList.Contains(getRegionInfo.EnglishName)))
+                if (!(cultureList.Contains(getRegionInfo.EnglishName)))
                 {
-                    CultureList.Add(getRegionInfo.EnglishName);
+                    cultureList.Add(getRegionInfo.EnglishName);
                 }
             }
 
             // sorting array
-            CultureList.Sort();
+            cultureList.Sort();
 
             // returning country list
-            return CultureList;
+            return cultureList;
         }
 
         //creating contact list
 
         public void ContactHistory()
         {
-            CustomerNotes customerNotes = new CustomerNotes(txtAddNote.Text);
-            this.currentCustomer.Notes.Add(customerNotes);
-            DataContainer.Update(this.currentCustomer);
-            txtAddNote.Text = "";
-            updateNotesView();
+            try
+            {
+                Validation.Required(txtAddNote, "Notizfeld muss ausgefüllt werden!");
+                CustomerNotes customerNotes = new CustomerNotes(txtAddNote.Text);
+                this._currentCustomer.Notes.Insert(0, customerNotes);
+                DataContainer.Update(this._currentCustomer);
+                txtAddNote.Text = "";
+                UpdateNotesView();
+            }
+            catch (ValidationException ex)
+            {
+                MessageBox.Show(ex.Message, "Validierungsfehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void generateErrorMessage(string errorMessage)
-        {
-           MessageBox.Show(errorMessage, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-
-        private void cleanUpFields()
+        private void CleanUpFields()
         {
             CmbSalutation.SelectedIndex = -1;
             txtFirstName.Clear();
@@ -127,95 +132,105 @@ namespace Contact_Manager.Partials.Dialog
             CmbCustomerType.SelectedIndex = -1;
         }
 
-        private void createCustomer()
+        private void CreateCustomer()
         {
-            /* *****************************
-            * declare vars
-           ***************************** */
-
-            int selectedGender;
-            string defaultCountry = "Sitzerland";
-            
-
-            /* *****************************
-             * formatting stuff
-            ***************************** */
-            int zipCodeFormatted = Convert.ToInt32(txtZipCode.Text);
-
-
-            /* *****************************************************
-             * check which gender is selected and store in variable
-             * male = 1 / female = 2 / other = 3
-            ***************************************************** */
-
-            if (rbMale.Checked)
-                selectedGender = 1;
-            else if (rbFemale.Checked)
-                selectedGender = 2;
-            else
-                selectedGender = 3;
-
-            /* *****************************
-            * check input for invalid data
-           ***************************** */
-
-            // check if required fields are filled out
-            if (txtFirstName.Text.Length < 1 || txtSurName.Text.Length < 1 || txtAddress.Text.Length < 1 || txtCompany.Text.Length < 1)
-                generateErrorMessage("Dieses Feld muss ausgefüllt werden.");
-
-            // check if email is correct
-            // source: https://stackoverflow.com/questions/5342375/regex-email-validation / https://docs.microsoft.com/en-us/dotnet/api/system.net.mail.mailaddress?redirectedfrom=MSDN&view=net-5.0
             try
             {
-                MailAddress m = new MailAddress(txtCompanyContactEmail.Text);
+                /* *****************************
+                * declare vars
+               ***************************** */
+
+                int selectedGender;
+                string defaultCountry = "Sitzerland";
+
+
+                /* *****************************
+                 * formatting stuff
+                ***************************** */
+                int zipCodeFormatted = Convert.ToInt32(txtZipCode.Text);
+
+
+                /* *****************************************************
+                 * check which gender is selected and store in variable
+                 * male = 1 / female = 2 / other = 3
+                ***************************************************** */
+
+                if (rbMale.Checked)
+                    selectedGender = 1;
+                else if (rbFemale.Checked)
+                    selectedGender = 2;
+                else
+                    selectedGender = 3;
+
+                /* *****************************
+                * check input for invalid data
+               ***************************** */
+
+                // check if required fields are filled out
+                Validation.Required(txtFirstName.Text, "Feld Vorname muss ausgefüllt werden.");
+                Validation.Required(txtSurName.Text, "Feld Nachname muss ausgefüllt werden.");
+                Validation.Required(txtAddress.Text, "Feld Adresse muss ausgefüllt werden.");
+                Validation.Required(txtCompany.Text, "Feld Unternehmen muss ausgefüllt werden.");
+
+                // check if email is correct
+                Validation.ValidateEmail(txtCompanyContactEmail.Text);
+
+                // check if zip code is valid for switzerland
+                Validation.ValidateZipCode(zipCodeFormatted);
+
+                // TODO: Kundentypen wie ?
+
+
+                // check phone / fax number lengths
+                Validation.ValidatePhone(txtPhonePrivate.Text, "Die Privatnummer ist nicht gültig!");
+                if (txtFax.Text.Length > 0)
+                {
+                    Validation.ValidatePhone(txtFax.Text, "Die Faxnummer ist nicht gültig!");
+                }
+
+                Validation.ValidatePhone(txtPhoneCompany.Text, "Die Geschäftsnummer ist nicht gültig!");
+                if (txtMobile.Text.Length > 0)
+                {
+                    Validation.ValidatePhone(txtMobile.Text, "Die Handynummer ist nicht gültig!");
+                }
+
+                /* *****************************
+                * create customer object
+               ***************************** */
+
+
+                Customer customer = new Customer(
+                    salutation: CmbSalutation.Text,
+                    firstName: txtFirstName.Text,
+                    lastName: txtSurName.Text,
+                    dateOfBirth: DtpDateOfBirth.Value,
+                    gender: selectedGender,
+                    title: CmbTitle.Text,
+                    email: txtCompanyContactEmail.Text,
+                    status: ChkStatus.Checked,
+                    address: txtAddress.Text,
+                    zipCode: zipCodeFormatted,
+                    phonePrivate: txtPhonePrivate.Text,
+                    phoneCompany: txtPhoneCompany.Text,
+                    fax: txtFax.Text,
+                    mobile: txtMobile.Text,
+                    city: txtCity.Text,
+                    country: defaultCountry,
+                    companyName: txtCompany.Text,
+                    customerType: CmbCustomerType.Text,
+                    notes: new List<CustomerNotes>()
+                );
+
+                DataContainer.AddModel(DataContainer.Customers, customer);
             }
-            catch (FormatException)
+            catch (ValidationException ex)
             {
-                generateErrorMessage("Das Format der E-Mail Adresse ist ungültig.");
+                MessageBox.Show(ex.Message, "Validierungsfehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
-            // check if zip code is valid for switzerland
-            if (zipCodeFormatted < 1000 || zipCodeFormatted > 9999)
-                generateErrorMessage("Die Postleitzahl ist zu klein / gross.");
-
-            //Kundentypen wie ?
-
-
-
-            // check phone / fax number lengths
-            if (txtPhonePrivate.Text.Length < 1 || txtPhonePrivate.Text.Length > 12 || txtPhoneCompany.Text.Length < 1 ||
-                txtPhoneCompany.Text.Length > 12 || txtMobile.Text.Length < 1 || txtMobile.Text.Length > 12 || txtFax.Text.Length < 1 ||
-                txtFax.Text.Length > 12)
-                generateErrorMessage("Bitte Telefon- / Fax-Nummer im folgenden Format angeben \"+41711234566\".");
-
-            /* *****************************
-            * create customer object
-           ***************************** */
-
-
-            Customer customer = new Customer(
-                salutation: CmbSalutation.Text,
-                firstName: txtFirstName.Text,
-                lastName: txtSurName.Text,
-                 dateOfBirth: DtpDateOfBirth.Value,
-                gender: selectedGender,
-                title: CmbTitle.Text,
-                email: txtCompanyContactEmail.Text,
-                status: ChkStatus.Checked,
-                address: txtAddress.Text,
-                zipCode: zipCodeFormatted,
-                phonePrivate: txtPhonePrivate.Text,
-                phoneCompany: txtPhoneCompany.Text,
-                fax: txtFax.Text,
-                mobile: txtMobile.Text,
-                city: txtCity.Text,
-                country: defaultCountry,
-                companyName: txtCompany.Text,
-                customerType: CmbCustomerType.Text,
-                notes: new List<CustomerNotes>()
-            );
-
-            DataContainer.AddModel(DataContainer.Customers, customer);
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnCompanySave_Click(object sender, EventArgs e)
@@ -223,23 +238,33 @@ namespace Contact_Manager.Partials.Dialog
             /* *********************************
              * call function to create employee
             ********************************* */
-            createCustomer();
-            MessageBox.Show("Kunde wurde erfolgreich erstellt.");
-            cleanUpFields();
+            CreateCustomer();
+            MessageBox.Show("Kunde wurde erfolgreich erstellt.", "Erfolgreich", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
         }
 
         private void BtnAddNote_Click(object sender, EventArgs e)
         {
-            if(this.noteInEditMode)
+            if(this._noteInEditMode)
             {
-                // Edit
-                this.currentCustomer.Notes[rowIndex].Notes = txtAddNote.Text;
-                DataContainer.Update(this.currentCustomer);
-                rowIndex = 0;
-                noteInEditMode = false;
-                txtAddNote.Text = "";
-                BtnAddNote.Text = "Notiz hinzufügen";
-                GrpBoxNotes.Text = "Notiz (erstellen)";
+                try
+                {
+                    // Edit
+                    Validation.Required(txtAddNote, "Notizfeld muss ausgefüllt werden!");
+                    this._currentCustomer.Notes[_rowIndex].Notes = txtAddNote.Text;
+                    DataContainer.Update(this._currentCustomer);
+                    _rowIndex = 0;
+                    _noteInEditMode = false;
+                    txtAddNote.Text = "";
+                    BtnAddNote.Text = "Notiz hinzufügen";
+                    GrpBoxNotes.Text = "Notiz (erstellen)";
+                    UpdateNotesView();
+                }
+                catch (ValidationException ex)
+                {
+                    MessageBox.Show(ex.Message, "Validierungsfehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
                 return;
             }
 
@@ -249,7 +274,7 @@ namespace Contact_Manager.Partials.Dialog
 
         private void btnCompanyDelete_Click(object sender, EventArgs e)
         {
-            cleanUpFields();
+            CleanUpFields();
         }
 
         private void CustomerDialog_Load(object sender, EventArgs e)
@@ -271,14 +296,14 @@ namespace Contact_Manager.Partials.Dialog
 
         private void dataGridNotes_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            rowIndex = e.RowIndex;
-            txtAddNote.Text = this.currentCustomer.Notes[rowIndex].Notes;
-            this.noteInEditMode = true;
+            _rowIndex = e.RowIndex;
+            txtAddNote.Text = this._currentCustomer.Notes[_rowIndex].Notes;
+            this._noteInEditMode = true;
             BtnAddNote.Text = "Notiz speichern";
             GrpBoxNotes.Text = "Notiz (bearbeiten)";
         }
 
-        private void updateNotesView()
+        private void UpdateNotesView()
         {
             if (_source == null)
             {
@@ -286,8 +311,7 @@ namespace Contact_Manager.Partials.Dialog
                 dataGridNotes.DataSource = _source;
             }
 
-            var notes = from CustomerNotes n in currentCustomer.Notes
-                orderby n.CreatedAt descending 
+            var notes = from CustomerNotes n in _currentCustomer.Notes
                 select new
                 {
                     Notiz = n.Notes,
---
+++ Contact_Manager/Validation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Manager
+{
+    /**
+     * Handles our own exception type
+     */
+    class ValidationException : Exception
+    {
+        public ValidationException(string message) : base(message: message)
+        {
+
+        }
+    }
+    class Validation
+    {
+        /**
+         * Check if the email is valid
+         */
+        public static void ValidateEmail(string email, string exceptionMessage = "Die E-Mailadresse ist nicht gültig")
+        {
+            // source: https://stackoverflow.com/questions/5342375/regex-email-validation / https://docs.microsoft.com/en-us/dotnet/api/system.net.mail.mailaddress?redirectedfrom=MSDN&view=net-5.0
+            try
+            {
+                new MailAddress(email);
+            }
+            catch (FormatException)
+            {
+                throw new ValidationException(exceptionMessage);
+            }
+        }
+
+        public static void ValidatePhone(string number, string exceptionMessage = "Die Nummer ist nicht gültig!")
+        {
+            // Trim the input
+            number = String.Concat(number.Where(c => !Char.IsWhiteSpace(c)));
+
+            // Check with 0041 00 000 00 00
+            if (number.Substring(0, 2) == "00" && (number.Length == 13 || number.Length == 14))
+            {
+                return;
+            }
+
+            // Check with +41 00 000 00 00
+            if (number.Substring(0, 1) == "+" && (number.Length == 12 || number.Length == 13))
+            {
+                return;
+            }
+
+            // Check with 000 000 00 00
+            if (number.Length == 10)
+            {
+                return;
+            }
+
+            throw new ValidationException(exceptionMessage + "\nFormate:\n0041 00 000 00 00\n +41 00 000 00 00\n000 000 00 00");
+        }
+
+        public static void ValidateZipCode(int zipCode, string exceptionMessage = "Die Postleitzahl ist nicht gültig!")
+        {
+            if (zipCode < 1000 || zipCode > 9999)
+            {
+                throw new ValidationException(exceptionMessage);
+            }
+        }
+
+        public static void Required(TextBox field, string exceptionMessage)
+        {
+            if (field.Text.Length < 1)
+            {
+                throw new ValidationException(exceptionMessage);
+            }
+        }
+
+        public static void Required(string field, string exceptionMessage)
+        {
+            if (field.Length < 1)
+            {
+                throw new ValidationException(exceptionMessage);
+            }
+        }
+    }
+}
